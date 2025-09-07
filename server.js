7894c78da61b90d07afba57a274138f1d420d89c
Aprealpx <joyaprilyntacugue30@gmail.com>
2025-09-07 15:08:06 +0800

    Update server.js

--- server.js
+++ server.js
@@ -7,10 +7,6 @@ const session = require('express-session');
 
 const app = express();
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
-
 const uploadDir = path.join(__dirname, 'uploads');
 const worksFile = path.join(__dirname, 'works.html');
 
@@ -109,4 +105,5 @@ app.listen(3000, () => {
 // Handle 404 errors for undefined routes
 app.use((req, res, next) => {
     res.status(404).send('404 Not Found');
+
 });
